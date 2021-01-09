d7c9ea905f5fc9de5c919c6652804f4cec680a5c
Life1223pl <daw_pl@interia.pl>
2021-01-09 09:33:58 +0100

    Finished

--- zad3/Interfaces/listQueue.php
+++
@@ -1,9 +0,0 @@
-<?php
-namespace Src\interfaces;
-
-interface listQueue{
-    public function SortByAlphabet();
-    public function SortByPoints();
-    public function SortRandom();
-    public function SortByDate();
-}
