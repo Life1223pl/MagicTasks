8c0dbdb3cc596321b4af50475084f7e4e7706c34
Life1223pl <daw_pl@interia.pl>
2021-01-20 09:39:51 +0100

    zad4/5 part 1 finished

---
+++ zad4/index.php
@@ -0,0 +1,53 @@
+<?php
+
+require_once __DIR__.'/dbconfig.php';
+
+class API{
+    function SetStatus($status,$user_email){
+        $db = new Connect();
+        $data = $db->prepare('UPDATE user SET status = :status WHERE email = :user_email');
+        $data->execute([':user_email' => $user_email,':status' => $status]);
+        echo 'Status for user '.$user_email." changed to ".$status;
+    }
+    function SelectPointsSaldo($email){
+        $db = new Connect();
+        $data = $db->prepare('Select * from user where email = :email');
+        $data->execute(['email' => $email]);
+       $outPutData = $data->fetch();
+       echo "Actual points for user: ".$email." = ".$outPutData['points'];
+    }
+
+    function ChangePoints($points,$user_email){
+        $db = new Connect();
+        $data = $db->prepare('UPDATE user SET points = :points WHERE email = :user_email');
+        $data->execute([':user_email' => $user_email,':points' => $points]);
+        echo 'Saldo Changed';
+
+    }
+    function CreateUser($email,$description,$points,$status){
+        $db = new Connect();
+        $data = $db->prepare('Insert INTO user values(null,:email,:description,:points,:status);');
+        $data->execute([':email' => $email, ':description' => $description, ':points' => $points, ':status' => $status]);
+        echo 'User Added';
+    }
+    function SelectAll(){
+        $db = new Connect();
+        $users = array();
+        $data = $db->prepare('Select * from user order by id');
+        $data->execute();
+        while ($outPutData = $data->fetch(PDO::FETCH_ASSOC)){
+            $users[$outPutData['Id']] = array(
+                'id' => $outPutData['Id'],
+                'email' => $outPutData['email'],
+                'description' => $outPutData['description'],
+                'points' => $outPutData['points'],
+                'status' => $outPutData['status']);
+        }
+        return json_encode($users);
+    }
+
+}
+
+$API = new API();
+header('Content-Type: application/json');
+echo $API->SelectAll();
