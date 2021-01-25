a2ad2b091b66409fad0bdfb0d2b354e15746e978
Life1223pl <daw_pl@interia.pl>
2021-01-25 08:54:14 +0100

    zad5 finished

--- zad4/index.php
+++ zad4/index.php
@@ -1,11 +1,16 @@
 <?php
 
 use App\Api;
-
+require_once 'vendor/autoload.php';
 require_once realpath("vendor/autoload.php");
 
+$loader = new \Twig\Loader\FilesystemLoader('templates');
+$twig = new \Twig\Environment($loader);
 
+$api = new Api();
+$result = $api->SelectAll();
 
-$Api = new Api();
-header('Content-Type: application/json');
-echo $Api->SelectAll();
+echo $twig->render('MasterTemplate.html',[]);
+echo $twig->render('ListingTemplate.html',[
+    'result' => $result
+]);
